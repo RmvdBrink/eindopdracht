cb132a19efd26a2a3fe69f90a1756aa6c31fe1bc
RmvdBrink <rmb1312@mail.com>
2022-12-22 20:42:17 +0100

    aanpassingen aan sign in

--- src/pages/sign-in/SignIn.js
+++ src/pages/sign-in/SignIn.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import "./SignIn.css"
+import "../../App.css"
 
-function SignIn(props) {
+
+function SignIn() {
     return (
-        <div>
-            <h1>Sign in</h1>
-        </div>
+        <>
+
+            <div  className="outer-content-container-sign-in">
+                <div className="inner-content-container-sign-in">
+                    <fieldset className="content-container-sign-in">
+
+                    <form className="sign-in-form">
+                        <p><strong>Sign In</strong> </p>
+                        <label htmlFor="email-field">
+                            Emailadres:
+                            <input
+                                type="email"
+                                id="email-field"
+                                name="email"
+                                // value={email}
+                                // onChange={(e) => setEmail(e.target.value)}
+                                placeholder="rick@novi.nl"
+                            />
+                        </label>
+                        <label htmlFor="password-field">
+                            Wachtwoord:
+                            <input
+                                type="password"
+                                id="password-field"
+                                name="password_field"
+                                // value={password}
+                                // onChange={(e) => setPassword(e.target.value)}
+                                placeholder="password"
+                            />
+                        </label>
+                        <button
+                            className="sign-in-button"
+                            type="submit"
+
+
+                        >Sign in</button>
+                        <p>no account yet click on sign up to create a new one</p>
+                        <p>forgot you password</p>
+                    </form>
+                    </fieldset>
+                        {/*<p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>*/}
+
+
+                </div>
+            </div>
+
+
+
+
+
+
+        </>
     );
 }
 
