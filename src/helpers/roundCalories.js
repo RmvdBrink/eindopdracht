1c9989d1626a8c33c9bf6af4565487afaff44813
RmvdBrink <rmb1312@mail.com>
2022-12-22 20:30:02 +0100

    sign in basis op gezet

---
+++ src/helpers/roundCalories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RoundFunction({ roundCalories}) {
+    function roundCalories(calories) {
+        return Math.round(calories);
+    }
+
+    return (
+        <div></div>
+    );
+}
+
+export default RoundFunction;
