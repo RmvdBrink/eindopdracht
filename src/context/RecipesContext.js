e1802a0d32f87eef986befd867d02446ce36c1b2
RmvdBrink <rmb1312@mail.com>
2023-01-17 13:58:15 +0100

    context voor recipe gemaakt

---
+++ src/context/RecipesContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+
+export const RecipesContext = createContext([]);
+
+export const RecipesProvider = ({ children }) => {
+    const [recipes, setRecipes] = useState([]);
+
+    return (
+        <RecipesContext.Provider value={{
+            recipes,
+            setRecipes,
+        }}>
+            {children}
+        </RecipesContext.Provider>
+    );
+}
